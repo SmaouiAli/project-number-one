ff2edb1cc7d4b8093b57e3baf8fda7bb8e07325e
SmaouiAli <smaouiali22@gmail.com>
2020-08-18 15:53:34 +0200

    added the button that will alloww me to add a dropdown menu

--- main.js
+++ main.js
@@ -2,4 +2,8 @@
 $(".welcome").append("<p id='mess'>Remember Gamers it is not obout the Skill it's all about the Gaming Chair</p>")// just a side message to bring intention
 $("#mess").append("<br><br><p id='introducing'>What Kind of games you like to play !</p>")
 $("#introducing").append("<br><p id ='list'>Choose from the list below</p>")
-$("#list").append("<div class ='buttons'><br><button id ='b1'>Action/Adventure</button> <button id ='b2'>Sports</button> <button id ='b3'>Chill</button> <button id ='b4'>Simulation</button> <button id ='b5'>Kids</button></div>")
+
+
+function displaySaM(){
+
+}
