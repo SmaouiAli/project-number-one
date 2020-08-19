5fe31ea91026dbdfe4c30d0745d2aaa8af6e8314
SmaouiAli <smaouiali22@gmail.com>
2020-08-19 02:12:24 +0200

    added 4 preferences eachone has its own list of options

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 
-$(".welcome").append("<p id='mess'>Remember Gamers it is not obout the Skill it's all about the Gaming Chair</p>")// just a side message to bring intention
-$("#mess").append("<br><br><p id='introducing'>What Kind of games you like to play !</p>")
-$("#introducing").append("<br><p id ='list'>Choose from the list below</p>")
 
 
-function displaySaM(){
 
-}
+
